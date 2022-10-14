48b8391718a218aa7542aafcb5893a7bca023077
Vgalassi <viniciusgalassi@gmail.com>
2022-10-14 17:34:47 -0300

    main

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,25 @@
+<?php
+use App\http\Controllers\ProfessorController;
+use App\http\Controllers\AlunosController;
+use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('trueindex');
+});
+
+Route::resource('alunos',AlunosController::class);
+
+Route::resource('professores',ProfessorController::class);
+
+//xd
